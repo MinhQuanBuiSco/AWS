1c00704a8267755c5736a7e785e0e2616ce53d7c
MinhQuanBuiSco <quanbm2710@gmail.com>
2025-04-02 20:53:09 +0900

    change dir

--- api-gateway-bedrock-ip-restriction-waf-codepipeline/api_gateway_bedrock_ip_restriction_waf_codepipeline/pipeline_stack.py
+++ api-gateway-bedrock-ip-restriction-waf-codepipeline/api_gateway_bedrock_ip_restriction_waf_codepipeline/pipeline_stack.py
@@ -27,10 +27,9 @@ class PipelineStack(Stack):
                 "cd api-gateway-bedrock-ip-restriction-waf-codepipeline",
                 "pip install -r requirements.txt",
                 "npm install -g aws-cdk",
-                "cdk synth",
-                "ls"
+                "cdk synth"
             ],
-            primary_output_directory="api-gateway-bedrock-ip-restriction-waf-codepipeline/"
+            primary_output_directory="api-gateway-bedrock-ip-restriction-waf-codepipeline/cdk.out"
         )
 
         # Pipeline
